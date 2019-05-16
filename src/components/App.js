2c3651f87feb7052362a5fee9064f307370660e9
PAWAN JAIN <jainpawan211199@gmail.com>
2019-05-16 21:06:47 +0530

    added semantic grid class

--- src/components/App.js
+++ src/components/App.js
@@ -26,12 +26,21 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-          videos={this.state.videos} 
-          onVideoSelect={this.onVideoSelect} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList 
+                videos={this.state.videos} 
+                onVideoSelect={this.onVideoSelect} 
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
